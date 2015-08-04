9e1b39b12e2da83ca4747a92cece513587e3a0b4
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2015-08-04 19:37:36 +0300

    Modify the print statements, now compatible with both Python 2 and 3
    Also set the output json keys to be sorted

--- mc_stats_converter.py
+++ mc_stats_converter.py
@@ -12,19 +12,19 @@ mappingsfilename = 'mappings.txt'
 printpretty = False
 
 def print_help():
-    print "Usage: " + sys.argv[0] + " [--pretty] [--mappings=path/to/mappingsfile] <statsdir>"
-    print ""
-    print "This program will convert the Minecraft stats files from the 1.7 format (numerical IDs)"
-    print "to the 1.8 format (string IDs)."
-    print "You must give it a 'mappings file' that contains all the id -> string mappings."
-    print "Such a mappings file should be available with this program at the original repository."
-    print ""
-    print "The '--pretty' option will also output nicely formatted versions of the stats files"
-    print "so that you can better see if everything seems right."
-    print ""
-    print "v1.0 @ 2015-01-14"
-    print "by masa"
-    print "Available at https://github.com/maruohon/mcstatsconverter"
+    print ("Usage: %s [--pretty] [--mappings=path/to/mappingsfile] <statsdir>" % sys.argv[0])
+    print ("")
+    print ("This program will convert the Minecraft stats files from the 1.7 format (numerical IDs)")
+    print ("to the 1.8 format (string IDs).")
+    print ("You must give it a 'mappings file' that contains all the id -> string mappings.")
+    print ("Such a mappings file should be available with this program at the original repository.")
+    print ("")
+    print ("The '--pretty' option will also output nicely formatted versions of the stats files")
+    print ("so that you can better see if everything seems right.")
+    print ("")
+    print ("v1.0 @ 2015-01-14")
+    print ("by masa")
+    print ("Available at https://github.com/maruohon/mcstatsconverter")
 
 if len(sys.argv) > 1:
     gotdir = False
@@ -39,13 +39,13 @@ if len(sys.argv) > 1:
                 print_help()
                 exit()
             else:
-                print "Unrecognized option '" + s + "'"
+                print ("Unrecognized option '%s'" % s)
                 exit(1)
         elif gotdir == False:
             workdir = s
             gotdir = True
         else:
-            print "Unrecognized/extraneous parameter '" + s + "'"
+            print ("Unrecognized/extraneous parameter '%s'" % s)
             exit(1)
 else:
     print_help()
@@ -54,10 +54,10 @@ else:
 if workdir[len(workdir) - 1] != os.sep:
     workdir = workdir + os.sep
 
-# print "mappingsfile: " + mappingsfilename
+# print ("mappingsfile: " + mappingsfilename)
 
 if os.path.isfile(mappingsfilename) == False:
-    print "Error: Could not find the mappings file '" + mappingsfilename + "'"
+    print ("Error: Could not find the mappings file '%s'" % mappingsfilename)
     exit(1)
 
 keymappings = {}
@@ -70,7 +70,7 @@ with open(mappingsfilename, 'r') as mappingsfile:
             keyid = match.group(1)
             keystr = match.group(2)
             keystrmod = keystr.replace('minecraft:', 'minecraft.')
-            # print "match: " + keyid + " => " + keystrmod + " (" + keystr + ")"
+            # print ("match: %s => %s (%s)" % (keyid, keystrmod, keystr))
             keymappings[keyid] = keystrmod
 
 
@@ -90,7 +90,7 @@ def convert_statsfile(path, filename, keymappings):
     newstats = {}
 
     for oldkey in oldstats:
-        # print "stat: " + oldkey
+        # print ("stat: %s" % oldkey)
         newkey = oldkey
 
         startpos = 0
@@ -108,11 +108,11 @@ def convert_statsfile(path, filename, keymappings):
             oldid = oldkey[startpos:len(oldkey)]
 
             if not oldid in keymappings:
-                print "Error: missing mapping for id '" + oldid + "'"
+                print ("Error: missing mapping for id '%s'" % oldid)
                 exit(1)
 
             newkey = oldkey[0:startpos] + keymappings[oldid]
-            # print "mapping key: " + oldkey + " => " + newkey
+            # print ("mapping key: %s => %s" % (oldkey, newkey))
 
         newstats[newkey] = oldstats[oldkey]
 
@@ -126,14 +126,14 @@ def convert_statsfile(path, filename, keymappings):
             json.dump(newstats, sorted, sort_keys=True, indent=4)
 
     statsfile = open(workdir + filename, 'w')
-    json.dump(newstats, statsfile)
+    json.dump(newstats, statsfile, sort_keys=True)
     statsfile.close
 
 
 
 for filename in os.listdir(workdir):
     if filename.endswith(".json"):
-        print "backing up file: " + filename
+        print ("backing up file: %s" % filename)
         backup_statsfile(workdir, filename)
-        print "converting file: " + filename
+        print ("converting file: %s" % filename)
         convert_statsfile(workdir, filename, keymappings)
